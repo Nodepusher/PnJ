bc0f4a12e189ad3a19792aae6af7124fc64a7e49
dev-imoongs <dev.imoong@gmail.com>
2024-05-21 20:12:53 +0900

    임포트 수정

--- frontend/src/Pages/PostDetailPage.js
+++ frontend/src/Pages/PostDetailPage.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getPostData } from '../store/postDetailReducer';
-import HeaderContainer from '../Containers/List/HeaderContainer';
 import DetailFooterContainer from '../Containers/Detail/DetailFooterContainer';
 import DetailSectionContainer from '../Containers/Detail/DetailSectionContainer';
+import HeaderContainer from '../Containers/Common/HeaderContainer';
 
 const PostDetailPage = () => {
     const dispatch = useDispatch();
