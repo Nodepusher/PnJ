f956c32eb0a4938e1522cd8f203c9a02e5055362
code-hyun <jh940412@gmail.com>
2024-06-03 17:50:51 +0900

    no message

--- frontend/src/store/postWriteReducer.js
+++ frontend/src/store/postWriteReducer.js
@@ -2,15 +2,22 @@ import axios from "axios";
 
 // action type 정의
 const POSTING_DATA = 'post/POSTING_DATA';
+const UPDATE_INPUT_DATA = 'post/UPDATE_INPUT_DATA';
 
 
 // action 생성
+// 1. 서버와 통신
 export const savePostData = () => {
     return async() => {
         
     }
 
   };
+// 2. update post data
+export const updatePostData = (inputData) => ({
+  type : UPDATE_INPUT_DATA,
+  payload : inputData,
+})
 
 // 초기 스테이트
 const initialState = {
@@ -20,7 +27,7 @@ const initialState = {
         content : '',
         category : '',
         tags : '',
-        image : '',
+        // image : '',
         file : ''
     }
   };
